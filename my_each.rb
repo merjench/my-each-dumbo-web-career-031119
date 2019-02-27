83ec2600cc744c9b5318d2cb69f7cb79f8329412
Merjen Charyyeva <merjen.chary@gmail.com>
2019-02-27 20:24:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,20 @@
 array = [1,2,3,4]
 
-def my_each (array) do |i|
-  puts i 
+def my_each (array) 
+  i = 0
+  while i < array.lenght
+  yield(array[i])
+  i+=1 
+end 
+  array
 end 
  
- 
- while i < array.lenght
- yield (array[i])
- i+=1 
+
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
-array 
-end 
