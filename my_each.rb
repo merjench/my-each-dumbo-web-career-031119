14c28de5675c27afd402ddf5017fd8acdb2a1720
Merjen Charyyeva <merjen.chary@gmail.com>
2019-02-27 20:26:11 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,8 @@
-array = [1,2,3,4]
-
-def my_each (array) do |i|
-  puts i 
-end 
- 
- 
- while i < array.lenght
- yield (array[i])
- i+=1 
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
 end
-array 
-end 
